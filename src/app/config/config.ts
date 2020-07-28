9349cf5c8fc1a038b5b22dbc30b7760d1e8193fb
José Panero <panerootero@gmail.com>
2020-07-28 23:39:53 +0200

    Cambios en archivo 'config.ts'

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
 export const config = { 
-    EMAIL_USER: 'SU_CORREO',
-    API_KEY: 'AIzaSyCAvRsOB4z5ZIN8SW7N6AjFq1hcjBCkevw'
+    EMAIL_USER: '<your-email-user>',
+    API_KEY: '<your-api-key>'
 }
